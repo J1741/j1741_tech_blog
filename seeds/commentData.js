77abc61680f2a34dd7ef09134c9aabeb1f7be2e6
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-24 15:21:39 -0500

    adding skeletons to seed data files

---
+++ seeds/commentData.js
@@ -0,0 +1,12 @@
+const { Comment } = require('../models');
+
+const commentdata = [
+  {},
+  {},
+  {},
+  {}
+];
+
+const seedComments = () => Comment.bulkCreate(commentdata);
+
+module.exports = seedComments;
---
+++ seeds/userData.js
@@ -0,0 +1,10 @@
+const { User } = require('../models');
+
+const userdata = [
+  {},
+  {},
+];
+
+const seedUsers = () => User.bulkCreate(userdata);
+
+module.exports = seedUsers;
