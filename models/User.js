24d4cf7135271a95a93cce5d00a493ff713c08b5
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-24 14:16:22 -0500

    adding Post model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
-  // checks password
+  // compares hashes of stored pw and login pw
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
