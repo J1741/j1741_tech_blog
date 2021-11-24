1af28069780ff6eca733417c2066cd596c587db7
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-24 16:17:28 -0500

    fixing a misspelling in User moadel that was causing an error during seeding; successfully tested seeding database

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ User.init(
   {
     // hooks to hash password provided by user 
     hooks: {
-      async befereCreate(newUserData) {
+      async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
