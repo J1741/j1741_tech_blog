2dca4c3c290c521f5d11660b3f0394799cc79ddb
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-24 20:48:14 -0500

    successfully rendering all post titles on the homepage; adding some custom styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,43 @@
 const router = require('express').Router();
+const {Post, User} = require('../models');
+// const withAuth = require('../utils/auth');
 
-// test home route
-router.get('/', (req, res) => {
+// test homeroute
+router.get('/test', (req, res) => {
   res.send("hello there!")
 });
 
-// test handlebars route
-router.get('/hbr', (req, res) => {
-  res.render('all-posts');
-})
+// // homeroute to see all posts
+// router.get('/', (req, res) => {
+//   res.render('all-posts');
+// })
+
+// TESTING: homeroute to see all posts
+router.get('/', async (req, res) => {
+  console.log('** GET / homeroute hit **')
+  try {
+    const allPosts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+
+    const posts = allPosts.map((post) => post.get({
+      plain: true
+    }));
+
+    console.log("** serialized posts:", posts);
+
+    res.render('all-posts', {
+      posts,
+      // logged_in: req.session.logged_in
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 module.exports = router;
