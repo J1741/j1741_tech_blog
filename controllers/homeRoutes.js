e3f91fc7ecd260d557589e247f8d58a133887e51
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-24 19:52:24 -0500

    successfully rendered test handlebars page via home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
   res.send("hello there!")
 });
 
+// test handlebars route
+router.get('/hbr', (req, res) => {
+  res.render('all-posts');
+})
+
 module.exports = router;
